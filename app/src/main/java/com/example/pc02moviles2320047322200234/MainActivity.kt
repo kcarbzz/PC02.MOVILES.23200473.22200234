d85d12fea99097a2fd3c492b13ea3fe5cccdbe1b
Leyinc1 <manuelipenza@gmail.com>
2025-11-22 08:45:56 -0500

    Inicialización de proyceto y creación de carpetas basicas

---
+++ app/src/main/java/com/example/pc02moviles2320047322200234/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pc02moviles2320047322200234
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pc02moviles2320047322200234.ui.theme.PC02MOVILES2320047322200234Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PC02MOVILES2320047322200234Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PC02MOVILES2320047322200234Theme {
+        Greeting("Android")
+    }
+}
